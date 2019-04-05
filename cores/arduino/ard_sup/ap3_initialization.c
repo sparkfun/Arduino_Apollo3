02c4a5db4a87e6763302307a7469689b288bbd1a
Owen L - SFE <oclyke@gmail.com>
2019-04-05 15:31:55 -0600

    create ap3 initialization files to handle common boot-up

---
+++ cores/arduino/ard_sup/ap3_initialization.c
@@ -0,0 +1,16 @@
+#include "ap3_initialization.h"
+
+void ap3_init( void ){
+    // Set the clock frequency.
+    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+
+    // Set the default cache configuration
+    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    am_hal_cachectrl_enable();
+
+    // Configure the board for low power operation.
+    am_bsp_low_power_init();
+
+    // Initialize the printf interface for ITM/SWO output.
+    am_bsp_uart_printf_enable(); // Enable UART - will set debug output to UART
+}
