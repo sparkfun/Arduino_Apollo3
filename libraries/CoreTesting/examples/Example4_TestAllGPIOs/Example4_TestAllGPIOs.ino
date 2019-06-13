589da6f7980fccb09932aad7cabdd5b2c44deb74
Nathan Seidle <nathan@sparkfun.com>
2019-06-12 21:18:57 -0600

    Clean up of examples

--- libraries/CoreTesting/examples/Example4_TestAllGPIOs/Example4_TestAllGPIOs.ino
+++ libraries/CoreTesting/examples/Example4_TestAllGPIOs/Example4_TestAllGPIOs.ino
@@ -6,39 +6,61 @@
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
 
-  This blinks a given IO pin to verify we have digitalWrite
-  control of every pin. 
-  
+  This blinks alternating pins to verify we have digitalWrite
+  control of every pin (and no shorts between pins). Useful for initial hardware testing but
+  otherwise a pretty boring sketch.
+
   Feel like supporting open source hardware?
   Buy a board from SparkFun!
   SparkFun Edge: https://www.sparkfun.com/products/15170
 
   Hardware Connections:
-  Connect an Edge via FTDI programmer
+  Connect an Edge via serial programmer
   Upload code
   Verify GPIOs are going high/low every 2 seconds
- */
+*/
 
-#define testPin 33
-
-#define LED 5 //On board LED is on pin 5
+byte pinsToToggle = 0;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("SparkFun Arduino Apollo3");
+  Serial.begin(9600);
 
-  pinMode(testPin, OUTPUT);
-  pinMode(LED, OUTPUT);
+#ifdef ARDUINO_AM_AP3_SFE_BB_ARTEMIS
+  Serial.println("SparkFun BlackBoard Artemis");
+  pinsToToggle = 30;
+#elif AM_AP3_SFE_BB_ARTEMIS_NANO 
+  Serial.println("SparkFun BlackBoard Artemis Nano");
+  pinsToToggle = 20;
+#elif AM_AP3_SFE_BB_ARTEMIS_MEGA 
+  Serial.println("SparkFun BlackBoard Artemis Mega");
+  pinsToToggle = 50;
+#else
+  Serial.println("Unknown board.");
+  pinsToToggle = 10;
+#endif
+
+  Serial.printf("Toggling %d pins\n\r", pinsToToggle); //My goodness it's nice to have printf again
+  
+  Serial.println("pinMode will override the serial configuration so no more printing...");
+
+  for(int x = 0 ; x < pinsToToggle ; x++)
+  {
+    pinMode(x, OUTPUT);
+  }
 }
 
 void loop() {
-  Serial.println("Toggle");
-
-  digitalWrite(testPin, LOW);
-  digitalWrite(LED, LOW);
+  for(int x = 0 ; x < pinsToToggle ; x++)
+  {
+    if(x % 2 == 0) digitalWrite(x, LOW);
+    else digitalWrite(x, HIGH);
+  }
   delay(2000);
 
-  digitalWrite(testPin, HIGH);
-  digitalWrite(LED, HIGH);
+  for(int x = 0 ; x < pinsToToggle ; x++)
+  {
+    if(x % 2 == 0) digitalWrite(x, HIGH);
+    else digitalWrite(x, LOW);
+  }
   delay(2000);
 }
