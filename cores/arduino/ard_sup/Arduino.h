7a582b74390b333eead386e92201b0176b9e7e2f
Nathan Seidle <nathan.seidle@sparkfun.local>
2019-06-19 12:58:53 -0600

    Add defines for bit manipulation and low/high byte
    Adds support for various Arduino reference functions

--- cores/arduino/ard_sup/Arduino.h
+++ cores/arduino/ard_sup/Arduino.h
@@ -75,6 +75,16 @@ extern "C"
 #define degrees(rad) ((rad)*RAD_TO_DEG)
 #define sq(x) ((x) * (x))
 
+#define lowByte(w) ((uint8_t)((w)&0xff))
+#define highByte(w) ((uint8_t)((w) >> 8))
+
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+
+#define bit(b) (1UL << (b))
+
 // constants
 #define LOW (0x0)
 #define HIGH (0x1)
