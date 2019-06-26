ea036478c240fa93747cb3bb8a6c02b5bfa03e57
Nathan Seidle <nathan.seidle@sparkfun.local>
2019-06-26 17:04:40 -0600

    Initial push. Mostly working. Needs writeMicro and overloaded attach.

---
+++ libraries/Servo/src/Servo.cpp
@@ -0,0 +1,74 @@
+/*
+
+Copyright (c) 2015 Arduino LLC. All rights reserved.
+Copyright (c) 2019 SparkFun Electronics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "Servo.h"
+#include "Arduino.h"
+
+//Constructor
+Servo::Servo()
+{
+}
+
+//Assign global var
+void Servo::attach(uint8_t pinNumber)
+{
+	_servoPinNumber = pinNumber;
+	_servoPadNumber = ap3_gpio_pin2pad(pinNumber);
+}
+
+void Servo::write(uint8_t servoPosition)
+{
+	_servoPosition = servoPosition;
+	if (_servoPosition > 180)
+		_servoPosition = 180; //Bounds check
+
+	uint16_t newServoPosition = map(servoPosition, 0, 180, 0, 1023);
+
+	servoWrite(_servoPadNumber, newServoPosition); // This and the above write should both produce 1.5 ms wide pulses, though using different resolutions
+}
+
+void Servo::writeMicroseconds(uint8_t servoPosition)
+{
+
+	servoWrite(_servoPadNumber, newServoPosition); // This and the above write should both produce 1.5 ms wide pulses, though using different resolutions
+}
+
+void Servo::detach(void)
+{
+	_servoPinNumber = NULL;
+	_servoPadNumber = NULL;
+	pinMode(_servoPinNumber, INPUT); //Will stop PWM output
+}
+
+uint8_t Servo::read(void)
+{
+	return (_servoPosition);
+}
+
+bool Servo::attached(uint8_t pinNumber)
+{
+	if (_servoPinNumber == pinNumber)
+		return (true);
+	return (false);
+}
