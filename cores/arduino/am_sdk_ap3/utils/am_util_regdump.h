6a5f1e355be482be5052ea888d408ab81b823b12
Owen L - SFE <oclyke@gmail.com>
2019-08-12 11:29:41 -0600

    Replace 'utils' by 2.2.0 version

--- cores/arduino/am_sdk_ap3/utils/am_util_regdump.h
+++ cores/arduino/am_sdk_ap3/utils/am_util_regdump.h
@@ -40,7 +40,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
-// This is part of revision 2.1.0 of the AmbiqSuite Development Package.
+// This is part of revision v2.2.0-7-g63f7c2ba1 of the AmbiqSuite Development Package.
 //
 //*****************************************************************************
 #ifndef AM_UTIL_REGDUMP_H
