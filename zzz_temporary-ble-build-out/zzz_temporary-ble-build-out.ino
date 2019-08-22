48caa15ca25820d8f6814d16ef86f4db18a53582
Owen L - SFE <owen.lyke@sparkfun.com>
2019-08-22 12:58:41 -0600

    Compiles with boot radio call

--- zzz_temporary-ble-build-out/zzz_temporary-ble-build-out.ino
+++ zzz_temporary-ble-build-out/zzz_temporary-ble-build-out.ino
@@ -1,8 +1,24 @@
 #include "wsf_types.h"
 #include "wsf_trace.h"
 #include "wsf_buf.h"
-
+#include "hci_handler.h"
+#include "dm_handler.h"
+#include "l2c_handler.h"
+#include "att_handler.h"
+#include "smp_handler.h"
+#include "l2c_api.h"
+#include "att_api.h"
+#include "smp_api.h"
+//#include "app_api.h" // oops, i deleted this one... let's see if we can get by without it
+#include "hci_core.h"
+#include "hci_drv.h"
 #include "hci_drv_apollo.h"
+#include "hci_drv_apollo3.h"
+
+#include "am_mcu_apollo.h"
+#include "am_util.h"
+
+
 
 void setup() {
   // put your setup code here, to run once:
