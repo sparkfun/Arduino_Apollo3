0cbfc9a45fc6b222f1ebee03f1a427e2e27343de
Nathan Seidle <nathan@sparkfun.com>
2019-10-16 18:51:39 -0600

    Add example to show Apollo3 IC revision

---
+++ libraries/Examples/examples/Advanced/DisplayICRevision/DisplayICRevision.ino
@@ -0,0 +1,42 @@
+/* Author: Nathan Seidle
+  Created: October 16th, 2019
+  License: MIT. See SparkFun Arduino Apollo3 Project for more information
+
+  This example demonstrates how to display the revision of the Apollo3.
+  See the Ambiq website for errata on each revision: https://ambiqmicro.com/mcu/
+*/
+void setup()
+{
+    Serial.begin(115200);
+    delay(10); //Wait for any bootloader UART interactions to complete
+    Serial.println();
+    Serial.print("Apollo3 IC revision code: ");
+
+    if (APOLLO3_A0)
+    {
+        Serial.print("A0");
+    }
+    else if (APOLLO3_A1)
+    {
+        Serial.print("A1");
+    }
+    else if (APOLLO3_B0)
+    {
+        Serial.print("B0");
+    }
+    else if (APOLLO3_GE_B0)
+    {
+        Serial.print("Unknown revision but it's greater than B0");
+    }
+    else
+    {
+        Serial.print("Unknown revision");
+    }
+    Serial.println();
+    Serial.println("All done");
+}
+
+void loop()
+{
+    //Do nothing
+}
