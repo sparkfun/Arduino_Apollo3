da71acbcc4bdc7a070756428dea7aa168b4b3480
Adam <adam.garbo@gmail.com>
2020-04-24 12:41:47 -0400

    Update RTC.h

--- libraries/RTC/src/RTC.h
+++ libraries/RTC/src/RTC.h
@@ -8,19 +8,18 @@ class APM3_RTC
 public:
   APM3_RTC();
 
-  void getTime(); //Query the RTC for the current time/date. Loads .seconds, .minute, etc.
+  void getTime(); //Query the RTC for the current time/date
   uint32_t getEpoch(); //Return the current RTC time/date as UNIX Epoch time 
   
   void setTime(uint8_t hund, uint8_t sec, uint8_t min, uint8_t hour, uint8_t dayOfMonth, uint8_t month, uint16_t year); //Set current time to provided hundredths/seconds/etc
   void setToCompilerTime(); //Set to time when sketch was compiled
-  void setEpoch(uint32_t ts); //Set RTC date time with provided UNIX Epoch time
+  void setEpoch(uint32_t ts); //Set current time to provided UNIX Epoch time
 
-  void getAlarm(); //Query the RTC for the current alarm time/date.
-  uint32_t getAlarmEpoch(); //Return the current RTC alarm time/date as UNIX Epoch time 
+  void getAlarm(); //Query the RTC for the current alarm time/date
   void setAlarm(uint8_t hund, uint8_t min, uint8_t sec, uint8_t hour, uint8_t dayOfMonth, uint8_t month); //Set alarm time to provided hundredths/seconds/etc
   void setAlarmMode(uint8_t mode); //Set the RTC alarm repeat interval
-  void attachInterrupt(); //Attach RTC alarm interrupt
-  void detachInterrupt(); //Detach RTC alarm interrupt
+  void attachInterrupt(); //Attach the RTC alarm interrupt
+  void detachInterrupt(); //Detach the RTC alarm interrupt
 
   uint32_t weekday; //0 to 6 representing the day of the week
   uint32_t century;
