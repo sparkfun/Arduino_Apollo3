c23d225552b8455ea3c0f4db51d66f82cffb050f
Adam <adam.garbo@gmail.com>
2020-06-03 11:03:30 -0400

    Update Example4_Set_Epoch.ino

--- libraries/RTC/examples/Example4_Set_Epoch/Example4_Set_Epoch.ino
+++ libraries/RTC/examples/Example4_Set_Epoch/Example4_Set_Epoch.ino
@@ -1,19 +1,13 @@
 /* 
-  Author: Adam Garbo
-  Created: May 23rd, 2020
+  Author: Adam Garbo and Nathan Seidle
+  Created: June 3rd, 2020
   License: MIT. See SparkFun Arduino Apollo3 Project for more information
 
   This example demonstrates how to set the RTC using UNIX Epoch time.
-  
-  Most SparkFun Artemis boards have the necessary external 32kHz crystal to
-  enable the RTC. If you are using the Artemis module bare you will either
-  need an external 32kHz xtal or use the internal LFRC. Read the datasheet
-  section 12.1 for more information.
-
 */
 
-#include "RTC.h" //Include RTC library included with the Aruino_Apollo3 core
-APM3_RTC myRTC; //Create instance of RTC class
+#include "RTC.h" // Include RTC library included with the Aruino_Apollo3 core
+APM3_RTC myRTC; // Create instance of RTC class
 
 void setup()
 {
@@ -21,12 +15,15 @@ void setup()
   Serial.println("SparkFun RTC Set UNIX Epoch Time Example");
 
   // Set the RTC time using UNIX Epoch time
-  myRTC.setEpoch(1590240780); // E.g. Saturday, May 23, 2020 1:33:00 PM
+  myRTC.setEpoch(1591185600); // E.g. 12:00:00, June 3rd, 2020 
 }
 
 void loop()
 {
+  // Print UNIX Epoch timestamp
   Serial.print("Epoch time: "); Serial.println(myRTC.getEpoch());
+  
+  // Print RTC's date and time
   Serial.print("Timestamp: "); printDateTime();
 
   delay(1000);
