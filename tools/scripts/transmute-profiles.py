4f9dc27f4f1d81e4ac9320d5192c9d6464efc8c7
oclyke <oclyke@gmail.com>
2020-06-29 12:38:32 -0600

    allow variant generation to handle blacklists

--- tools/scripts/transmute-profiles.py
+++ tools/scripts/transmute-profiles.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 
+blacklist_format = '{"flags": [], "macros": [], "symbols": []}'
 
 # ***********************************************************************************
 #
@@ -18,10 +19,37 @@ def main():
     with open(args.profile, 'r') as fin:
         profile = json.loads(fin.read())
 
+    blacklist = json.loads(blacklist_format)
+    if args.blacklist:
+        try:
+            with open(args.blacklist, 'r') as fin:
+                blacklist = json.loads(fin.read())
+            print('\tusing user-specified blacklist')
+            verboseprint('\t\t' + str(blacklist))
+        except FileNotFoundError as e:
+            print(e)
+        except:
+            print('error loading the blacklist file')
+
+    try:
+        blacklist['flags']
+    except KeyError:
+        blacklist['flags'] = []
+    try:
+        blacklist['macros']
+    except KeyError:
+        blacklist['macros'] = []
+    try:
+        blacklist['symbols']
+    except KeyError:
+        blacklist['symbols'] = []
+
     outpath = args.dest + '-flags'
     verboseprint('\twriting into ' + outpath)
     with open(outpath, 'w') as fout:
         for flag in profile['flags']:
+            if flag in blacklist['flags']:
+                continue
             output = flag + ' '
             verboseprint('\t\t' + output)
             fout.write(output)
@@ -30,6 +58,8 @@ def main():
     verboseprint('\twriting into ' + outpath)
     with open(outpath, 'w') as fout:
         for macro in profile['macros']:
+            if macro in blacklist['macros']:
+                continue
             output = macro
             verboseprint('\t\t' + output)
             fout.write(output)
@@ -38,6 +68,8 @@ def main():
     verboseprint('\twriting into ' + outpath)
     with open(outpath, 'w') as fout:
         for symbol in profile['symbols']:
+            if symbol in blacklist['symbols']:
+                continue
             output = '-D' + symbol + ' '
             verboseprint('\t\t' + output)
             fout.write(output)
@@ -58,6 +90,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-p', '--profile', dest='profile', required=True, help='path to profile to convert')
     parser.add_argument('-d', '--dest', dest='dest', required=True, help='path to output files - will be postfixed with extension')
+    parser.add_argument('-b', '--blacklist', dest='blacklist', help='path to JSON formatted blacklist for ' + blacklist_format)
     parser.add_argument('-v', '--verbose', default=0, help='enable verbose output', action='store_true')
 
     args = parser.parse_args()
