45d04919554cc65c00c703964e09321bd634bb9c
oclyke <oclyke@gmail.com>
2020-07-09 09:01:21 -0600

    initial gpio implementation via arduino-mbed bridge

---
+++ variants/SFE_ARTEMIS_DK/arduino/pins.cpp
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2020 SparkFun Electronics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "pins.h"
+
+const pin_size_t variantPinCount = 24;
+
+PinState variantPinStates[variantPinCount] = {
+    {D13, 13, NULL, /*NULL,*/ NULL},
+    {D16, 16, NULL, /*NULL,*/ NULL},
+    {D23, 23, NULL, /*NULL,*/ NULL},
+    {D24, 24, NULL, /*NULL,*/ NULL},
+    {D25, 25, NULL, /*NULL,*/ NULL},
+    {D26, 26, NULL, /*NULL,*/ NULL},
+    {D27, 27, NULL, /*NULL,*/ NULL},
+    {D28, 28, NULL, /*NULL,*/ NULL},
+    {D29, 29, NULL, /*NULL,*/ NULL},
+    {D31, 31, NULL, /*NULL,*/ NULL},
+    {D32, 32, NULL, /*NULL,*/ NULL},
+    {D33, 33, NULL, /*NULL,*/ NULL},
+    {D34, 34, NULL, /*NULL,*/ NULL},
+    {D35, 35, NULL, /*NULL,*/ NULL},
+    {D36, 36, NULL, /*NULL,*/ NULL},
+    {D37, 37, NULL, /*NULL,*/ NULL},
+    {D38, 38, NULL, /*NULL,*/ NULL},
+    {D39, 39, NULL, /*NULL,*/ NULL},
+    {D40, 40, NULL, /*NULL,*/ NULL},
+    {D41, 41, NULL, /*NULL,*/ NULL},
+    {D42, 42, NULL, /*NULL,*/ NULL},
+    {D43, 43, NULL, /*NULL,*/ NULL},
+    {D44, 44, NULL, /*NULL,*/ NULL},
+    {D45, 45, NULL, /*NULL,*/ NULL},
+};
