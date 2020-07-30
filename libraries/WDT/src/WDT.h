66b6e6e17fd9c5dcc71e5ade7a1f4d4827912f48
Adam <adam.garbo@gmail.com>
2020-07-30 10:03:40 -0400

    Update WDT.h

--- libraries/WDT/src/WDT.h
+++ libraries/WDT/src/WDT.h
@@ -8,5 +8,11 @@ class APM3_WDT
 public:
   APM3_WDT();
 
+  void configure(uint32_t clock, uint16_t interrupt, uint16_t reset); // Configure watchdog
+  void enable(); // Enable the watchdog
+  void disable(); // Disable the watchdog
+  void restart(); // Restart the watchdog
+  void clear() // Clear the watchdog interrupt
+
 };
 #endif //WDT_H
