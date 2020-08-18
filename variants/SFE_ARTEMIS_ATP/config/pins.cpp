c5a2b3afb8f9d5d982085243880279b109fae73e
oclyke <owen.lyke@sparkfun.com>
2020-08-17 22:54:13 -0600

    add artemis atp

---
+++ variants/SFE_ARTEMIS_ATP/config/pins.cpp
@@ -0,0 +1,57 @@
+/* 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.md', which is part of this source code package.
+*/
+
+#include "bridge/pins.h"
+
+const pin_size_t variantPinCount = 43;
+
+PinState variantPinStates[variantPinCount] = {
+    {D0, 0, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D1, 1, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D2, 2, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D3, 3, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D4, 4, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D5, 5, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D6, 6, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D7, 7, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D8, 8, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D9, 9, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D10, 10, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D11, 11, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D12, 12, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D13, 13, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D14, 14, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D15, 15, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D16, 16, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D17, 17, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D18, 18, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D19, 19, NULL, /*NULL, NULL, NULL,*/ NULL},
+    // {D20, 20, NULL, /*NULL, NULL, NULL,*/ NULL},
+    // {D21, 21, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D22, 22, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D23, 23, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D24, 24, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D25, 25, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D26, 26, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D27, 27, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D28, 28, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D29, 29, NULL, /*NULL, NULL, NULL,*/ NULL},
+    // {D30, 30, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D31, 31, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D32, 32, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D33, 33, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D34, 34, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D35, 35, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D36, 36, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D37, 37, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D38, 38, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D39, 39, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D40, 40, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D41, 41, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D42, 42, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D43, 43, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D44, 44, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {D45, 45, NULL, /*NULL, NULL, NULL,*/ NULL},
+};
