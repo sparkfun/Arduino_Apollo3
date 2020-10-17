4f5fb942c2e581a157618948304abc36da0f26c5
oclyke <owen.lyke@sparkfun.com>
2020-10-17 12:54:18 -0600

    add LED1 and fix pinNames

--- variants/SFE_ARTEMIS_MM_PB/config/pins.cpp
+++ variants/SFE_ARTEMIS_MM_PB/config/pins.cpp
@@ -5,18 +5,19 @@
 
 #include "bridge/pins.h"
 
-const pin_size_t variantPinCount = 40;
+const pin_size_t variantPinCount = 41;
 
 PinState variantPinStates[variantPinCount] = {
+    {LED1,          19, NULL, /*NULL, NULL, NULL,*/ NULL},
     {AUD_OUT,       18, NULL, /*NULL, NULL, NULL,*/ NULL}, // only unique pins here, so AUD_MCLK == AUD_OUT on this board
     {AUD_IN,        11, NULL, /*NULL, NULL, NULL,*/ NULL},
     {AUD_LRCLK,     36, NULL, /*NULL, NULL, NULL,*/ NULL},
     {AUD_BCLK,      37, NULL, /*NULL, NULL, NULL,*/ NULL},
-    {SCL,           39, NULL, /*NULL, NULL, NULL,*/ NULL},
-    {SDA,           40, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {I2C_SCL,       39, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {I2C_SDA,       40, NULL, /*NULL, NULL, NULL,*/ NULL},
     {I2C_INT,        2, NULL, /*NULL, NULL, NULL,*/ NULL},
-    {SCL1,           8, NULL, /*NULL, NULL, NULL,*/ NULL},
-    {SDA1,           9, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {I2C1_SCL,       8, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {I2C1_SDA,       9, NULL, /*NULL, NULL, NULL,*/ NULL},
     {BATT_VIN,      31, NULL, /*NULL, NULL, NULL,*/ NULL},
     {SDIO_CLK,       5, NULL, /*NULL, NULL, NULL,*/ NULL},
     {SDIO_CMD,       7, NULL, /*NULL, NULL, NULL,*/ NULL},
@@ -26,7 +27,7 @@ PinState variantPinStates[variantPinCount] = {
     {SDIO_DATA3,    23, NULL, /*NULL, NULL, NULL,*/ NULL},
     {SPI_SDI,       43, NULL, /*NULL, NULL, NULL,*/ NULL},
     {SPI_SDO,       38, NULL, /*NULL, NULL, NULL,*/ NULL},
-    {SPI_SCK,       42, NULL, /*NULL, NULL, NULL,*/ NULL},
+    {SPI_CLK,       42, NULL, /*NULL, NULL, NULL,*/ NULL},
     {SPI_CS,        41, NULL, /*NULL, NULL, NULL,*/ NULL},
     {A0,            32, NULL, /*NULL, NULL, NULL,*/ NULL},
     {A1,            35, NULL, /*NULL, NULL, NULL,*/ NULL},
