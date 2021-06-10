53f69d5bcd9dc1eed405f87f7cc5ae499a251cd1
Wenn0101 <wenner0101@gmail.com>
2021-06-10 07:04:46 -0600

    create artemis module initial commit

---
+++ variants/SFE_ARTEMIS_MODULE/variant.h
@@ -0,0 +1,16 @@
+/* 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.md', which is part of this source code package.
+*/
+
+#ifndef _VARIANT_H_
+#define _VARIANT_H_
+
+#include "Arduino.h"
+
+#include "config/pins.h"
+
+#define VARIANT_SPI_INTFCS  0
+#define VARIANT_WIRE_INTFCS 0
+
+#endif // _VARIANT_H_
