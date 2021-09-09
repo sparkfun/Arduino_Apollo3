be9a90d865cbc8f396b642174aadea4007d8ccd1
wenn0101 <wenner0101@gmail.com>
2021-09-09 16:32:47 -0600

    add default wire for artemis module see comments for details

--- variants/SFE_ARTEMIS_MODULE/variant.h
+++ variants/SFE_ARTEMIS_MODULE/variant.h
@@ -10,7 +10,13 @@
 
 #include "config/pins.h"
 
-#define VARIANT_SPI_INTFCS  0
-#define VARIANT_WIRE_INTFCS 0
+#define VARIANT_SPI_INTFCS 0
+#define VARIANT_WIRE_INTFCS 1
+
+// Add a default wire pin so that Wire is available to libraries and sketches
+// Since this is just a module no pins are labeled I2C/Wire/Qwiic by default
+// 2 pins have been chosen here arbitrarily
+#define VARIANT_Wire_SDA 9
+#define VARIANT_Wire_SCL 8
 
 #endif // _VARIANT_H_
